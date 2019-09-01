282a8c93c897a25a80c14a4b334a406152f59509
JDMR - lentiummmx <lentium.mmx@gmail.com>
2019-09-01 16:21:19 -0500

    Changes committed to fix typos

--- src/parsers/fxp.js
+++ src/parsers/fxp.js
@@ -23,7 +23,7 @@ class FXP {
         // tagValueProcessor : a => he.decode(a) //default is a=>a
     };
 
-    #DEFAUL_TO_XML_OPTIONS = {
+    #DEFAULT_TO_XML_OPTIONS = {
         attributeNamePrefix: "@_",
         // attrNodeName: "@", //default is false
         textNodeName: "#text",
@@ -32,7 +32,7 @@ class FXP {
         cdataPositionChar: "\\c",
         format: false,
         indentBy: "  ",
-        supressEmptyNode: false,
+        suppressEmptyNode: false,
         // tagValueProcessor: a=> he.encode(a, { useNamedReferences: true}),// default is a=>a
         // attrValueProcessor: a=> he.encode(a, {isAttributeValue: isAttribute, useNamedReferences: true})// default is a=>a
     };
@@ -50,7 +50,7 @@ class FXP {
     }
 
     toXml() {
-        const parser = new this.XmlParser(this.#DEFAUL_TO_XML_OPTIONS);
+        const parser = new this.XmlParser(this.#DEFAULT_TO_XML_OPTIONS);
         return parser.parse(this.#json);
     }
 }
