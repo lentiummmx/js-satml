9dcf08b36eea77028a66e60256188a5bb5918aee
JDMR - lentiummmx <lentium.mmx@gmail.com>
2019-09-20 22:52:11 -0500

    Revert "FTR-004 Changes committed to add eslint rules and code improvement"
    This reverts commit 5b95b1cbe3839dcd8fda66f8200e0b1dc95e85a4.

--- src/parsers/fxp.js
+++ src/parsers/fxp.js
@@ -1,63 +1,58 @@
-import FastXmlParser from 'fast-xml-parser';
+import fast_xml_parser from 'fast-xml-parser';
 
 class FXP {
-  #xml;
 
-  #json;
+    #xml;
+    #json;
 
-  #DEFAULT_TO_JSON_OPTIONS = {
-    attributeNamePrefix: '@_',
-    // attrNodeName: "attr", //default is 'false'
-    textNodeName: '#text',
-    ignoreAttributes: false,
-    ignoreNameSpace: false,
-    allowBooleanAttributes: false,
-    parseNodeValue: true,
-    parseAttributeValue: false,
-    trimValues: true,
-    cdataTagName: '__cdata', // default is 'false'
-    cdataPositionChar: '\\c',
-    localeRange: '', // To support non english character in tag/attribute values.
-    parseTrueNumberOnly: false,
-    // attrValueProcessor: a => he.decode(a, {isAttributeValue: true}),//default is a=>a
-    // tagValueProcessor : a => he.decode(a) //default is a=>a
-  };
+    #DEFAULT_TO_JSON_OPTIONS = {
+        attributeNamePrefix: "@_",
+        // attrNodeName: "attr", //default is 'false'
+        textNodeName: "#text",
+        ignoreAttributes: false,
+        ignoreNameSpace: false,
+        allowBooleanAttributes: false,
+        parseNodeValue: true,
+        parseAttributeValue: false,
+        trimValues: true,
+        cdataTagName: "__cdata", //default is 'false'
+        cdataPositionChar: "\\c",
+        localeRange: "", //To support non english character in tag/attribute values.
+        parseTrueNumberOnly: false,
+        // attrValueProcessor: a => he.decode(a, {isAttributeValue: true}),//default is a=>a
+        // tagValueProcessor : a => he.decode(a) //default is a=>a
+    };
 
-  #DEFAULT_TO_XML_OPTIONS = {
-    attributeNamePrefix: '@_',
-    // attrNodeName: "@", //default is false
-    textNodeName: '#text',
-    ignoreAttributes: false,
-    cdataTagName: '__cdata', // default is false
-    cdataPositionChar: '\\c',
-    format: false,
-    indentBy: '  ',
-    supressEmptyNode: false,
-    // tagValueProcessor: a=> he.encode(a, { useNamedReferences: true}),
-    // ^^ default is a=>a
-    // attrValueProcessor: a=> he.encode(a, {
-    //   isAttributeValue: isAttribute, useNamedReferences: true
-    // })
-    // ^^ default is a=>a
-  };
+    #DEFAULT_TO_XML_OPTIONS = {
+        attributeNamePrefix: "@_",
+        // attrNodeName: "@", //default is false
+        textNodeName: "#text",
+        ignoreAttributes: false,
+        cdataTagName: "__cdata", //default is false
+        cdataPositionChar: "\\c",
+        format: false,
+        indentBy: "  ",
+        supressEmptyNode: false,
+        // tagValueProcessor: a=> he.encode(a, { useNamedReferences: true}),// default is a=>a
+        // attrValueProcessor: a=> he.encode(a, {isAttributeValue: isAttribute, useNamedReferences: true})// default is a=>a
+    };
 
-  JsonParser = FastXmlParser;
+    JsonParser = fast_xml_parser;
+    XmlParser = fast_xml_parser.j2xParser;
 
-  XmlParser = FastXmlParser.j2xParser;
+    constructor(opts) {
+        this.#xml = opts.xml || '';
+        this.#json = opts.json || {};
+    }
 
-  constructor(opts) {
-    this.#xml = opts.xml || '';
-    this.#json = opts.json || {};
-  }
+    toJson() {
+        return this.JsonParser.parse(this.#xml, this.#DEFAULT_TO_JSON_OPTIONS);
+    }
 
-  toJson() {
-    return this.JsonParser.parse(this.#xml, this.#DEFAULT_TO_JSON_OPTIONS);
-  }
-
-  toXml() {
-    const parser = new this.XmlParser(this.#DEFAULT_TO_XML_OPTIONS);
-    return parser.parse(this.#json);
-  }
+    toXml() {
+        const parser = new this.XmlParser(this.#DEFAULT_TO_XML_OPTIONS);
+        return parser.parse(this.#json);
+    }
 }
 
 export default FXP;
